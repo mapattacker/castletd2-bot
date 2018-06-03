ec2ba7666d4d27c7c3ee7a3888ab2f601392926e
Jake <xxx>
2018-06-03 22:56:24 +0800

    added OCR intelligence to the bot

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ There are two ways to overcome this. One is to install the mobile OS in a virtua
 
 The python library pyautogui help to simulate mouse-clicks on the window so that an entirely rule-based system based on clicking an x,y coordinate can be coded. This example uses stage **15-3** of the game **Castle TD2**. It will build and upgrade all towers to reach the last level in the stage but not completing it (completing a stage drastically reduces the amount of diamonds collected on subsequent tries). 
 
-Due to the highly customised nature, this example cannot be repeated on other stages or games. Image/video recognition together with machine learning will make the bot more powerful, but this is just a start. :)
+I have added a level of intelligence to the bot using object character recognition (OCR). The script takes screenshots of the game periodically and crop the image to a specific popup to detect if the game has ended so that it can auto restart.
+
+The next level of the bot is to employ image recognition so that I can command the bot to a next level of automation. Stay tune!
 
 ### Dependencies
-__Python__: pyautogui
+__Python__: pyautogui, pytesseract, pillow
 
 __Chrome Vysor__: mobile mirroring software
 
--- castletd.py
+++ castletd.py
@@ -1,85 +1,226 @@
 import pyautogui
 import time
+from PIL import Image
+import pytesseract
+import os
 
-speed = (546,99)
-list = [(304, 318), (360, 295), (401, 267), (447, 238), \
-        (381, 216), (310, 213), (231, 214), (160, 234), \
-        (202, 265), (260, 295), (473, 326), (523, 295), \
-        (121, 325), (80, 291)]
-upgrade = [(35,35),(0,-61),(33,-41),(0,-61)]
-hero = [(38, 146),(37, 197),(583, 142)]
-start = (582, 359)
 
-# select screen
+startall = time.time()
+# define initial OCR text variable
+text = '1'
+# speed button
+speed = (552, 98)
+# screenshot directory
+img_path = r'/Users/xxx/Desktop/screenshots/screen.png'
+screenshot = "screencapture {}".format(img_path)
+# cropping of screenshots
+wood = (70, 180, 150, 210)
+end_msg = (450, 300, 780, 360)
+# tower positions
+list = [(306, 293), (362, 269), (405, 244), (450, 218), \
+        (383, 201), (312, 197), (233, 202), (165, 219), \
+        (204, 245), (262, 272), (479, 298), (527, 269), \
+        (122, 297), (80, 267)]
+# upgrade to lasers
+upgrade = [(27,32),(0,-54),(29,-37),(0,-61)]
+hero = [(36, 183),(587, 136)]
+hero2 = (36, 138)
+start = (587, 331)
+# back button of vysor
+backbutton = (102, 418) # horizontal
+backbuttonV = (51, 708) # vertical
+cancelbutton = (253, 314)
+restartbutton =(361, 296)
+
+
+# select vysor screen
 pyautogui.click(list[0])
 
-# shift heros
-for i in hero:
-    pyautogui.click(i)
-    pyautogui.click(206, 309)
 
-# speed up
-pyautogui.click(speed)
-# pyautogui.click(start) round
-pyautogui.click(start)
-time.sleep(2)
 
-# 1st tower
-for i in list:
-    a = i
-    pyautogui.click(a)
-    time.sleep(0.5)
-    z = upgrade[0]
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(0.5)
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(0.15)
+def objectrecognition():
+    # screenshot to check for ending
+    os.system(screenshot)
+    img = Image.open(img_path)
+    cropped = img.crop(end_msg)
+    # declare OCR output variable as global
+    global text
+    text = pytesseract.image_to_string(cropped) # OCR to text
+
+
+def upgrade1st():
+    # 1st tower upgrade
+    for i in list:
+        a = i
+        pyautogui.click(a)
+        time.sleep(0.5)
+        z = upgrade[1]
+        pyautogui.click(a[0]+z[0],a[1]+z[1])
+        time.sleep(0.5)
+        pyautogui.click(a[0]+z[0],a[1]+z[1])
+        time.sleep(6)
+        pyautogui.click(start)
+
+        # OCR
+        objectrecognition()
+        if text == 'Emergency':
+            print(1, 'Game ended')
+            global end
+            end = 1
+            break
+        else:
+            pass
+
+
+def upgrade2nd():
+    # 2nd tower upgrade
+    global end
+    if end != 1:
+        for i in list:
+            a = i
+            pyautogui.click(a)
+            time.sleep(0.5)
+            z = upgrade[2]
+            pyautogui.click(a[0]+z[0],a[1]+z[1])
+            time.sleep(0.5)
+            pyautogui.click(a[0]+z[0],a[1]+z[1])
+            time.sleep(4)
+            pyautogui.click(start)
+            time.sleep(4)
+            pyautogui.click(start)
+
+            # OCR
+            objectrecognition()
+            if text == 'Emergency':
+                print(2, 'Game ended')
+                end = 1
+                break
+            else:
+                pass
+
+
+def finalupgrade():
+    # Final tower upgrade
+    global end
+    if end != 1:
+        for zz, i in enumerate(list):
+            # remove 2 towers upgrade to hasten the pace
+            if zz != 3 and zz != 4:
+                a = i
+                pyautogui.click(a)
+                time.sleep(0.5)
+                z = upgrade[3]
+                pyautogui.click(a[0]+z[0],a[1]+z[1])
+                time.sleep(0.5)
+                pyautogui.click(a[0]+z[0],a[1]+z[1])
+                time.sleep(6)
+                pyautogui.click(start)
+                time.sleep(6)
+                pyautogui.click(start)
+
+                # OCR
+                objectrecognition()
+                if text == 'Emergency':
+                    end = 1
+                    print(3, 'Game ended')
+                    break
+                else:
+                    pass
+                
+        
+
+def restart():
+    # cancel, remove ad, restart game
+
+    # begin restart if OCR finds game has ended
+    if end != 1:
+        for i in range(20):
+            objectrecognition()
+            if text == 'Emergency':
+                print(4, 'Game Ended')
+                # click cancel button when game ends
+                pyautogui.click(cancelbutton)
+                break
+            else:
+                time.sleep(5)
+                pass
+
+    restart = [backbutton, backbuttonV, backbuttonV, backbutton, backbutton, backbuttonV, backbuttonV, backbuttonV]
+
+    for a, i in enumerate(restart):
+        objectrecognition()
+        if text == 'Defeat':
+            # click restart button if it appears
+            print('restart button shown')
+            pyautogui.click(restartbutton)
+            break
+        else:
+            # wait for ad to appear after clicking cancel
+            if a == 0:
+                time.sleep(5)
+                pyautogui.click(i)
+            # for others, can just click fast at 1 sec rate
+            else:
+                time.sleep(2)
+                pyautogui.click(i)
+                time.sleep(3)
+
+    endt = time.time()
+    if num == 1:
+        print (num, 'round down,', 360*num, 'diamonds earned! after',round((endt-startt)/60,2), 'mins')
+    else:
+        print (num, 'rounds down,', 360*num, 'diamonds earned! after', round((endt-startt)/60,2), 'mins')
+
+
+
+for num in range(1, 5):
+    # flag for ending
+    global end
+    end = 0
+    startt = time.time()
+    # shift heros
+    for i in hero:
+        pyautogui.click(i)
+        pyautogui.click(181, 272)
+
+    pyautogui.click(hero2)
+    pyautogui.click(435, 266)
+
+    # speed up
+    pyautogui.click(speed)
+    # pyautogui.click(start) round
+    pyautogui.click(start)
+    time.sleep(2)
+
+    # build towers
+    for i in list:
+        a = i
+        pyautogui.click(a)
+        time.sleep(0.5)
+        z = upgrade[0]
+        pyautogui.click(a[0]+z[0],a[1]+z[1])
+        time.sleep(0.5)
+        pyautogui.click(a[0]+z[0],a[1]+z[1])
+        time.sleep(0.15)
+        pyautogui.click(start)
+
+    # click for next round
+    time.sleep(7.5)        
     pyautogui.click(start)
 
-time.sleep(15)
-pyautogui.click(start)
+    upgrade1st()
 
-for i in list:
-    a = i
-    pyautogui.click(a)
-    time.sleep(0.5)
-    z = upgrade[1]
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(0.5)
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(5)
+    # click for next round
+    time.sleep(7.5)        
     pyautogui.click(start)
 
-time.sleep(15)        
-pyautogui.click(start)
 
-for i in list:
-    a = i
-    pyautogui.click(a)
-    time.sleep(0.5)
-    z = upgrade[2]
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(0.5)
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(8)
-    pyautogui.click(start)
+    upgrade2nd()
+    finalupgrade()
+    restart()
 
-for i in list:
-    a = i
-    pyautogui.click(a)
-    time.sleep(0.5)
-    z = upgrade[3]
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(0.5)
-    pyautogui.click(a[0]+z[0],a[1]+z[1])
-    time.sleep(12)
-    pyautogui.click(start)
 
+
+endall = time.time()
+print('game ended after {} mins'.format(round((endall-startall)/60,2)))
 # print(pyautogui.position())
-
-# cancel, remove ad, remove ad2, restart
-time.sleep(30)
-restart = [(237, 343),(327, 100),(24, 101),(366, 323)]
-for i in restart:
-    pyautogui.click(i)
-    time.sleep(7)
